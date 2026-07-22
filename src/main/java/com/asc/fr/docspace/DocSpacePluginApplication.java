00a2ad088b692dae614a1b98b2c0d2a0c599d501
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2026-07-22 14:57:57 +0500

    chore(application): added todo

--- src/main/java/com/asc/fr/docspace/DocSpacePluginApplication.java
+++ src/main/java/com/asc/fr/docspace/DocSpacePluginApplication.java
@@ -5,6 +5,7 @@ import com.fr.decision.fun.impl.AbstractHttpHandlerProvider;
 import com.fr.plugin.transform.FunctionRecorder;
 
 /** FineBI entry point: registers HTTP handlers from {@link DocSpacePluginApplicationContext}. */
+// TODO: Add Manual ReLink in v2?
 @FunctionRecorder
 public class DocSpacePluginApplication extends AbstractHttpHandlerProvider {
   @Override
