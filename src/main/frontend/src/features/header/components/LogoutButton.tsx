f563e6285f19e0d0ed266082c74412ccb00cbb49
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2026-07-29 17:14:44 +0500

    chore: use mono icon for nav and header buttons

--- src/main/frontend/src/features/header/components/LogoutButton.tsx
+++ src/main/frontend/src/features/header/components/LogoutButton.tsx
@@ -1,6 +1,6 @@
 import type { FunctionComponent } from "preact";
 
-import OnlyofficeIcon from "@resources/images/onlyoffice-logo.svg";
+import OnlyofficeIcon from "@resources/images/onlyoffice-logo_mono.svg";
 
 export const LogoutButton: FunctionComponent = () => (
   <OnlyofficeIcon width={20} height={20} aria-hidden="true" />
